157af0c40ce35e8452f72f0c572efced1331650a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2014-07-22 18:13:01 +0200

    Updated stuff.

--- improb-redux-example-2.r
+++ improb-redux-example-2.r
@@ -39,23 +39,33 @@ getexpectations.t2 = getconditionalexpectations(t2)
 isrbayesmaximal.t1 = ismaximalfunc(getexpectations.t1, rbayescompare)
 isrbayesmaximal.t2 = ismaximalfunc(getexpectations.t2, rbayescompare)
 # dT-T1
-isrbayesmaximal.t1(
+rvars = 
   c(-1, -1,
-    -6, 10)) 
+    -6, 10)
+getexpectations.t1(rvars)
+isrbayesmaximal.t1(rvars)
 # dT-T2
-isrbayesmaximal.t2(
+rvars =
   c(-1, -1,  # not optimal
-    -6, 10))
+    -6, 10)
+getexpectations.t2(rvars)
+isrbayesmaximal.t2(rvars)
 # dT
-isrbayesmaximal(
+rvars =
   c(-1, -1, -6, 10,
-    -6, 10, -6, 10))
+    -6, 10, -6, 10)
+getexpectations(rvars)
+isrbayesmaximal(rvars)
 # dTc
-isrbayesmaximal(
+rvars =
   c(0, 0, 0, 0, # not optimal
-    -5, 11, -5, 11))
+    -5, 11, -5, 11)
+getexpectations(rvars)
+isrbayesmaximal(rvars)
 # root
-isrbayesmaximal(
+rvars =
   c(-1, -1, -6, 10,
     -6, 10, -6, 10, # not optimal
-    -5, 11, -5, 11))
+    -5, 11, -5, 11)
+getexpectations(rvars)
+isrbayesmaximal(rvars)
