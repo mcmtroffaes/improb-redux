19cd1917fa96d36e22d0370b752ba889e4638ac5
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2014-07-22 15:53:17 +0200

    Investment example.

---
+++ examples/ex5.r
@@ -0,0 +1,25 @@
+source("../improb-redux.r")
+pmfs = c(
+  0, 0.6, 0.4,
+  0.3, 0.3, 0.4)
+getexpectations = getexpectationsfunc(3, pmfs) # 3 = size of possibility space
+getlowerprevisions = getlowerprevisionsfunc(getexpectations)
+getupperprevisions = getupperprevisionsfunc(getexpectations)
+isgammamaximin = isgammamaxisomethingfunc(getlowerprevisions)
+isgammamaximax = isgammamaxisomethingfunc(getupperprevisions)
+isbayesmaximal = ismaximalfunc(getexpectations, bayescompare)
+isintervalmaximal = ismaximalfunc(getexpectations, intervalcompare)
+isrobustbayes = isrobustbayesfunc(getexpectations)
+rvars = c(
+  100, 50, -25,
+  75, 50, 0,
+  60, 55, 10,
+  35, 35, 35)
+getexpectations(rvars)
+getlowerprevisions(rvars)
+getupperprevisions(rvars)
+isgammamaximin(rvars)
+isgammamaximax(rvars)
+isintervalmaximal(rvars)
+isbayesmaximal(rvars)
+isrobustbayes(rvars)
