63b0a8833e9157f39d73ba40e4172ba77dfd3ca4
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2014-07-22 15:39:25 +0200

    Solution to exercise 3.

---
+++ examples/ex3.r
@@ -0,0 +1,24 @@
+source("../improb-redux.r")
+pmfs = c(
+  0.6, 0.4,
+  0.65, 0.35)
+getexpectations = getexpectationsfunc(2, pmfs) # 2 = size of possibility space
+getlowerprevisions = getlowerprevisionsfunc(getexpectations)
+getupperprevisions = getupperprevisionsfunc(getexpectations)
+isgammamaximin = isgammamaxisomethingfunc(getlowerprevisions)
+isgammamaximax = isgammamaxisomethingfunc(getupperprevisions)
+isbayesmaximal = ismaximalfunc(getexpectations, bayescompare)
+isintervalmaximal = ismaximalfunc(getexpectations, intervalcompare)
+isrobustbayes = isrobustbayesfunc(getexpectations)
+rvars = c(
+  440, 260,
+  420, 300,
+  370, 370)
+getexpectations(rvars)
+getlowerprevisions(rvars)
+getupperprevisions(rvars)
+isgammamaximin(rvars)
+isgammamaximax(rvars)
+isintervalmaximal(rvars)
+isbayesmaximal(rvars)
+isrobustbayes(rvars)
