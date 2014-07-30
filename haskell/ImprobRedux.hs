648aad47b6659e3431f928029278b6b1c333ce00
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2014-07-30 17:05:41 +0100

    More testing, add type constraints in type signatures.

--- haskell/ImprobRedux.hs
+++ haskell/ImprobRedux.hs
@@ -7,19 +7,27 @@ module ImprobRedux (
   isgammamaxisomething,
   ) where
 
-lift2 f xs ys = map (\y -> map (\x -> f x y) xs) ys
+lift2 :: (a -> b -> c) -> [a] -> [b] -> [[c]]
+lift2 f xs ys = map (\x -> map (\y -> f x y) ys) xs
+expectation :: Num a => [a] -> [a] -> a
 expectation pmf rvar = sum $ zipWith (*) pmf rvar
 expectations = lift2 expectation
+conditionalpmf :: Fractional a => [Bool] -> [a] -> [a]
 conditionalpmf event pmf = map (/ norm) restrictedpmf
   where restrictedpmf = map fst $ filter snd $ zip pmf event
         norm = sum restrictedpmf
 conditionalexpectation event pmf = expectation (conditionalpmf event pmf)
 conditionalexpectations event = lift2 (conditionalexpectation event)
+mapexpectations :: ([a] -> a) -> ([[a]] -> [[a]]) -> [[a]] -> [a]
 mapexpectations f exps rvars = map f $ exps rvars
+lowerprevisions :: Ord a => ([[a]] -> [[a]]) -> [[a]] -> [a]
 lowerprevisions = mapexpectations minimum
+upperprevisions :: Ord a => ([[a]] -> [[a]]) -> [[a]] -> [a]
 upperprevisions = mapexpectations maximum
+hurwicz :: (Num a, Ord a) => a -> [a] -> a
 hurwicz opt xs = opt * (maximum xs) + (1 - opt) * (minimum xs)
 hurwiczprevisions opt = mapexpectations (hurwicz opt)
+isgammamaxisomething :: (Num a, Ord a) => a -> ([[a]] -> [a]) -> [[a]] -> [Bool]
 isgammamaxisomething tol f rvars = map ismax xs
   where xs = f rvars
         ismax x = x >= (maximum xs) - tol
--- haskell/Test.hs
+++ haskell/Test.hs
@@ -1,8 +1,36 @@
 module Test where
 
+import Data.Ratio
 import ImprobRedux
+import Test.HUnit
+
+test1 = TestCase $ do
+  assertEqual
+    "conditional expectation"
+    [[1, 0], [70 % 75, 5 % 75], [40 % 75, 35 % 75], [4 % 5, 1 % 5], [1, 0]]
+    (exps rvars)
+  assertEqual
+    "lower prevision"
+    [-1 % 3]
+    (lowerprevisions exps [[2, -3]])
+  assertEqual
+    "upper prevision"
+    [2]
+    (upperprevisions exps [[2, -3]])
+  where
+    pmfs = [
+      [ 3 % 10,   7 % 10,   0],
+      [25 % 100, 70 % 100,  5 % 100],
+      [25 % 100, 40 % 100, 35 % 100],
+      [ 5 % 10,   4 % 10,   1 % 10],
+      [ 5 % 10,   5 % 10,   0]]
+    event = [False, True, True]
+    rvars = [[1, 0], [0, 1]]
+    exps = conditionalexpectations event pmfs
 
 main = do
+  runTestTT $ TestList [test1]
+  {-
   putStrLn $ show $ expectations [[0.1, 0.9], [0.5, 0.5]] [[2, 3], [4, 1]]
   putStrLn $ show $ conditionalexpectations
     [True, False, True] [[0.1, 0.7, 0.2], [0.2, 0.5, 0.3]] [[3, 4], [2, 0]]
@@ -16,3 +44,4 @@ main = do
         upr = upperprevisions exps
         isgammamaximax = isgammamaxisomething 1e-10 lpr
         isgammamaximin = isgammamaxisomething 1e-10 upr
+  -}
