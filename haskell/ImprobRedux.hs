36366a4961fbaf0d46468ca5fd32bed247774387
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2014-07-30 15:56:13 +0100

    Avoid using exp from Prelude.

--- haskell/ImprobRedux.hs
+++ haskell/ImprobRedux.hs
@@ -6,7 +6,7 @@ conditionalpmf event pmf = map (/ norm) restrictedpmf
         norm = sum restrictedpmf
 conditionalexpectation event pmf = expectation (conditionalpmf event pmf)
 conditionalexpectations event = lift2 (conditionalexpectation event)
-mapexpectations f exp rvars = map f $ exp rvars
+mapexpectations f exps rvars = map f $ exps rvars
 lowerprevisions = mapexpectations minimum
 upperprevisions = mapexpectations maximum
 hurwicz opt xs = opt * (maximum xs) + (1 - opt) * (minimum xs)
@@ -24,13 +24,13 @@ main = do
     [True, False, True] [0.1, 0.7, 0.2] [3, 4]
   putStrLn $ show $ conditionalexpectations
     [True, False, True] [[0.1, 0.7, 0.2], [0.2, 0.5, 0.3]] [[3, 4], [2, 0]]
-  putStrLn $ show $ lowerprevisions exp [[2, 3], [4, 1]]
-  putStrLn $ show $ upperprevisions exp [[2, 3], [4, 1]]
-  putStrLn $ show $ hurwiczprevisions 0.5 exp [[2, 3], [4, 1]]
+  putStrLn $ show $ lowerprevisions exps [[2, 3], [4, 1]]
+  putStrLn $ show $ upperprevisions exps [[2, 3], [4, 1]]
+  putStrLn $ show $ hurwiczprevisions 0.5 exps [[2, 3], [4, 1]]
   putStrLn $ show $ isgammamaximax [[2, 3], [4, 1]]
   putStrLn $ show $ isgammamaximin [[2, 3], [4, 1]]
-  where exp = expectations [[0.1, 0.9], [0.5, 0.5]]
-        lpr = lowerprevisions exp
-        upr = upperprevisions exp
+  where exps = expectations [[0.1, 0.9], [0.5, 0.5]]
+        lpr = lowerprevisions exps
+        upr = upperprevisions exps
         isgammamaximax = isgammamaxisomething 1e-10 lpr
         isgammamaximin = isgammamaxisomething 1e-10 upr
