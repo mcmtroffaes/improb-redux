df092a017131cdb063b9eb40a067f2f70984c383
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2014-07-31 16:14:25 +0100

    Add type signatures.

--- haskell/ImprobRedux.hs
+++ haskell/ImprobRedux.hs
@@ -27,8 +27,10 @@ conditionalpmf event pmf = map (/ norm) restrictedpmf
   where restrictedpmf = map fst $ filter snd $ zip pmf event
         norm = sum restrictedpmf
 
+conditionalexpectation :: Fractional a => [Bool] -> [a] -> [a] -> a
 conditionalexpectation event pmf = expectation (conditionalpmf event pmf)
 
+conditionalexpectations :: Fractional a => [Bool] -> [[a]] -> [[a]] -> [[a]]
 conditionalexpectations event = lift2 (conditionalexpectation event)
 
 mapexpectations :: ([a] -> a) -> ([[a]] -> [[a]]) -> [[a]] -> [a]
@@ -43,18 +45,22 @@ upperprevisions = mapexpectations maximum
 hurwicz :: (Num a, Ord a) => a -> [a] -> a
 hurwicz opt xs = opt * (maximum xs) + (1 - opt) * (minimum xs)
 
+hurwiczprevisions :: (Num a, Ord a) => a -> ([[a]] -> [[a]]) -> [[a]] -> [a]
 hurwiczprevisions opt = mapexpectations (hurwicz opt)
 
-isgammamaxisomething :: (Num a, Ord a) => a -> ([[a]] -> [a]) -> [[a]] -> [Bool]
+isgammamaxisomething :: (Num a, Ord a) => a -> (b -> [a]) -> b -> [Bool]
 isgammamaxisomething tol f rvars = map ismax xs
   where xs = f rvars
         ismax x = x >= (maximum xs) - tol
 
+rbayesdominates :: (Num a, Ord a) => a -> [a] -> [a] -> Bool
 rbayesdominates tol xs ys = all cmp $ zip xs ys
   where cmp (x, y) = x > y + tol
 
+intervaldominates :: (Num a, Ord a) => a -> [a] -> [a] -> Bool
 intervaldominates tol xs ys = minimum xs > maximum ys + tol
 
+ismaximal :: (a -> a -> Bool) -> (b -> [a]) -> b -> [Bool]
 ismaximal dominates exps rvars = map (not . isdominated) table
   where table = exps rvars
         isdominated x = any (`dominates` x) table
