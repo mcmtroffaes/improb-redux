a4b3075fd4ec7360f260988ac9756d64ee0dd628
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2014-07-31 16:03:47 +0100

    Spacing between functions.

--- haskell/ImprobRedux.hs
+++ haskell/ImprobRedux.hs
@@ -15,32 +15,46 @@ module ImprobRedux (
 
 lift2 :: (a -> b -> c) -> [a] -> [b] -> [[c]]
 lift2 f xs ys = map (\y -> map (\x -> f x y) xs) ys
+
 expectation :: Num a => [a] -> [a] -> a
 expectation pmf rvar = sum $ zipWith (*) pmf rvar
+
 expectations :: Num a => [[a]] -> [[a]] -> [[a]]
 expectations = lift2 expectation
+
 conditionalpmf :: Fractional a => [Bool] -> [a] -> [a]
 conditionalpmf event pmf = map (/ norm) restrictedpmf
   where restrictedpmf = map fst $ filter snd $ zip pmf event
         norm = sum restrictedpmf
+
 conditionalexpectation event pmf = expectation (conditionalpmf event pmf)
+
 conditionalexpectations event = lift2 (conditionalexpectation event)
+
 mapexpectations :: ([a] -> a) -> ([[a]] -> [[a]]) -> [[a]] -> [a]
 mapexpectations f exps rvars = map f $ exps rvars
+
 lowerprevisions :: Ord a => ([[a]] -> [[a]]) -> [[a]] -> [a]
 lowerprevisions = mapexpectations minimum
+
 upperprevisions :: Ord a => ([[a]] -> [[a]]) -> [[a]] -> [a]
 upperprevisions = mapexpectations maximum
+
 hurwicz :: (Num a, Ord a) => a -> [a] -> a
 hurwicz opt xs = opt * (maximum xs) + (1 - opt) * (minimum xs)
+
 hurwiczprevisions opt = mapexpectations (hurwicz opt)
+
 isgammamaxisomething :: (Num a, Ord a) => a -> ([[a]] -> [a]) -> [[a]] -> [Bool]
 isgammamaxisomething tol f rvars = map ismax xs
   where xs = f rvars
         ismax x = x >= (maximum xs) - tol
+
 rbayesdominates tol xs ys = all cmp $ zip xs ys
   where cmp (x, y) = x > y + tol
+
 intervaldominates tol xs ys = minimum xs > maximum ys + tol
+
 ismaximal dominates exps rvars = map (not . isdominated) table
   where table = exps rvars
         isdominated x = any (`dominates` x) table
